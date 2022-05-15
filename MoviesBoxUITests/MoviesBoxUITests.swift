38265c919136058c83efafd4ed8079494c4bd892
Ahmed Ali <>
2022-05-15 17:16:33 +0200

    add swift lint script and apply linting on project files

--- MoviesBoxUITests/MoviesBoxUITests.swift
+++ MoviesBoxUITests/MoviesBoxUITests.swift
@@ -10,6 +10,7 @@ import XCTest
 class MoviesBoxUITests: XCTestCase {
 
     override func setUpWithError() throws {
+        try? super.setUpWithError()
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
         // In UI tests it is usually best to stop immediately when a failure occurs.
@@ -19,6 +20,7 @@ class MoviesBoxUITests: XCTestCase {
     }
 
     override func tearDownWithError() throws {
+        try? super.tearDownWithError()
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
--- MoviesBoxUITests/MoviesBoxUITestsLaunchTests.swift
+++ MoviesBoxUITests/MoviesBoxUITestsLaunchTests.swift
@@ -14,6 +14,7 @@ class MoviesBoxUITestsLaunchTests: XCTestCase {
     }
 
     override func setUpWithError() throws {
+        try? super.setUpWithError()
         continueAfterFailure = false
     }
 
