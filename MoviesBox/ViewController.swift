dbdf33756339e97590c98adcdc8d159e821f9686
Ahmed Ali <>
2022-05-15 16:20:22 +0200

    add initial project

---
+++ MoviesBox/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviesBox
+//
+//  Created by Ahmed Ali on 15/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
