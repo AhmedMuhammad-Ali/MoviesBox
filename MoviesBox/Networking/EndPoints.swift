557819bb61c99aa33cfcae7ef3725724ddc586a1
Ahmed Ali <>
2022-05-15 20:10:09 +0200

    add api end points

---
+++ MoviesBox/Networking/EndPoints.swift
@@ -0,0 +1,14 @@
+//
+//  EndPoints.swift
+//  MoviesBox
+//
+//  Created by Ahmed Ali on 15/05/2022.
+//
+
+import Foundation
+/// `EndPoints` contains all end points
+enum EndPoints {
+    case getEpisodes
+    case getChannels
+    case getCategories
+}
